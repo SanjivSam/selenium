1929189698b2b81baf4fdbc1ad6a95fa8fa57adf
Sanjiv <sanjiv.mellimi@gmail.com>
2018-03-19 17:39:24 +0530

    Binary Number - Count of consequitive ones Factorial Hour Glass Arry - Largest Sum TelephoneDirSearch

---
+++ training/src/test/java/module1/BinaryNumConseOnes.java
@@ -0,0 +1,33 @@
+package module1;
+
+import java.io.*;
+import java.util.*;
+
+public class BinaryNumConseOnes {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int result = binar(n);
+		System.out.println(result);
+	}
+
+	static int binar(int inte) {
+//		String arr = Integer.toBinaryString(inte);
+		String arr = Integer.toString(inte, 2);
+		String[] asd = arr.split("0");
+		int result=0;
+		for(int i=0;i<asd.length;i++) {
+			if(result<asd[i].length()) {
+				result=asd[i].length();
+			}
+		}
+		/*char[] arr1 = arr.toCharArray();
+		int count = 1;
+		for(int i=0;i<arr1.length-1;i++) {
+			if (arr1[i] == '1' && arr1[i + 1] == '1') 
+				count++;
+		}*/
+		return result;
+	}
+}
---
+++ training/src/test/java/module1/Factorial.java
@@ -0,0 +1,24 @@
+package module1;
+
+import java.util.*;
+
+public class Factorial {
+	static int temp = 1;
+
+	static int factorial(int n) {
+		if (n == 1)
+			temp = temp * n;
+		else if (n > 1)
+			temp = n * factorial(n - 1);
+		else
+			temp = 0;
+		return temp;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int result = factorial(n);
+		System.out.println(result);
+	}
+}
